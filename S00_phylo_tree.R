27915519ca53599f5a2ff8591dbeff9cb5aa6431
ibdj <ibdjacobsen@gmail.com>
2025-04-23 11:26:50 -0100

    updated to make phylo tree based on GBIF phylogony

--- S00_phylo_tree.R
+++ S00_phylo_tree.R
@@ -3,31 +3,39 @@
 if (!requireNamespace("pacman", quietly = TRUE) || !requireNamespace("devtools", quietly = TRUE)) {
   install.packages(c("pacman", "devtools"))
 }
+pacman::p_load(remotes, ape,tidyverse,googlesheets4, rgbif, ids, lubridate, devtools,V.PhyloMaker) 
 devtools::install_github("inbo/inborutils")
 install_github("jinyizju/V.PhyloMaker", dependencies = TRUE)
-pacman::p_load(remotes, ape,tidyverse,googlesheets4, rgbif, ids, lubridate, devtools,V.PhyloMaker) 
+
 
 ####### reading data #############################################################################################
 
+#importing the ALL names used in the NERO data
 sp.names <- read_csv("data/index_species.csv") |> 
   mutate(trait = func_type)
 
+#making a list of the species in the specific dataset and joining to get the full mames
 sp.list <- read_csv("data/data_2007.csv") |> left_join(sp.names, by = "taxon_code") |> 
   mutate(name = species)
 
 #### match species by gbif ########################################################################################
 
+#matching the data to GBIF to get species, family and genus (Your species list should include columns for `species`, `genus`, and `family`)
 sp.list <- sp.list |> name_backbone_checklist("name")
 
-#Your species list should include columns for `species`, `genus`, and `family`. 
+# removingany duplicates
 sp.list <-sp.list[!duplicated(sp.list$scientificName), ]
 
+# making sure that the list only contains the three needed variables
 sp.list <- sp.list[, c("species", "genus", "family")]  
 
-
-
-#### phylo maker ####
+#### phylo maker #################################################################################################
 result <- phylo.maker(sp.list, scenarios = "S3")
-plot(result$scenario.3)
+plot(result$scenario.3, ces = 0.5)
 
-result
+tree <- result$scenario.3
+
+write.tree(tree, file = "data/nero_phylo_tree.tree")
+
+
+
