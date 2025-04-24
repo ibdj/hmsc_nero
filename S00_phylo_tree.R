890b944bf978d912e8156c24b185457a8a4d8122
ibdj <ibdjacobsen@gmail.com>
2025-04-24 10:18:58 -0100

    added code to make the phylo tree with ape instead of V.Phylo

--- S00_phylo_tree.R
+++ S00_phylo_tree.R
@@ -1,12 +1,11 @@
 #### packages ####
 
 if (!requireNamespace("pacman", quietly = TRUE) || !requireNamespace("devtools", quietly = TRUE)) {
-  install.packages(c("pacman", "devtools"))
+  install.packages(c("pacman", "devtools","ape"))
 }
-pacman::p_load(remotes, ape,tidyverse,googlesheets4, rgbif, ids, lubridate, devtools,V.PhyloMaker) 
 devtools::install_github("inbo/inborutils")
-install_github("jinyizju/V.PhyloMaker", dependencies = TRUE)
-
+devtools::install_github("jinyizju/V.PhyloMaker")
+pacman::p_load(remotes,ape,tidyverse,googlesheets4,rgbif,ids,lubridate,devtools,V.PhyloMaker) 
 
 ####### reading data #############################################################################################
 
@@ -22,12 +21,17 @@ sp.list <- read_csv("data/data_2007.csv") |> left_join(sp.names, by = "taxon_cod
 
 #matching the data to GBIF to get species, family and genus (Your species list should include columns for `species`, `genus`, and `family`)
 sp.list <- sp.list |> name_backbone_checklist("name")
+full_list <- sp.list |> name_backbone_checklist("name")
 
 # removingany duplicates
 sp.list <-sp.list[!duplicated(sp.list$scientificName), ]
 
 # making sure that the list only contains the three needed variables
-sp.list <- sp.list[, c("species", "genus", "family")]  
+sp.list <- sp.list[, c("species", "genus", "family")]
+
+# format from the plant case study: 
+#species,family,genus,epithet
+#Abies concolor,Pinaceae,Abies,concolor
 
 #### phylo maker #################################################################################################
 result <- phylo.maker(sp.list, scenarios = "S3")
@@ -39,3 +43,11 @@ write.tree(tree, file = "data/nero_phylo_tree.tree")
 
 
 
+
+
+#### with ape only (like the plant case study) #######################
+sp.list <- sapply(sp.list, is.character)
+sp.list <- lapply(sp.list, as.factor)
+
+plant.tree = as.phylo(~family/genus/species, data=sp.list, collapse = FALSE)
+plant.tree$edge.length = rep(1,length(plant.tree$edge))
